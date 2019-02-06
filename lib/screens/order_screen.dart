04f6794145298b941b3e1bb815eec9da627abfa7
Alexandre <aacds@msn.com>
2019-02-06 11:13:38 -0200

    First commit

---
+++ lib/screens/order_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class OrderScreen extends StatelessWidget {
+
+  final String orderId;
+
+  OrderScreen(this.orderId);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pedido Realizado"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.check,
+              color: Theme.of(context).primaryColor,
+              size: 80.0,
+            ),
+            Text("Pedido realizado com sucesso!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 18.0,
+              ),
+            ),
+            Text("Código do produto: $orderId",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
